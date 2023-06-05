3cc013aed2678b56f4c679c098105835dbcd2f1b
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-06-05 22:40:24 +0300

    edit: add profile to app componenet

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Button from './components/Buttons';
+import st from './App.module.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className={st.app}>
+			<div className={st.profile}>
+				<div className={st.pic}>
+					<span>ДА</span>
+				</div>
+				<div className={st.info}>
+					<div className={st.name}>Джавлон Ахунджанов</div>
+					<div className={st.links}>
+						<a className={st.link} href='https://t.me/dxxbletriiiple' target='_blank'>
+							Telegram
+						</a>
+						<a className={st.link} href='https://github.com/dxxbletriiiple' target='_blank'>
+							GitHub
+						</a>
+						<a
+							className={st.link}
+							href='https://drive.google.com/file/d/1NVaoAoIU1SHOK22LlTlLYFtB4NIi6ewH/view'
+							target='_blank'
+						>
+							Resume
+						</a>
+					</div>
+				</div>
+			</div>
+			<hr className={st.devider} />
+			<form>
+				<Button text='' id='' handler={console.log} />
+			</form>
+		</div>
+	);
 }
 
-export default App
+export default App;
