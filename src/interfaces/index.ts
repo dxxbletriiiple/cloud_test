211029fd1e0ef9ad3295015eb79c15a9a2c73152
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-06-06 00:17:38 +0300

    add: input interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,5 +2,12 @@ export interface IButton {
 	text: string;
 	id: string;
 	isOutlined?: boolean;
-	handler: () => void;
+	handler: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export interface InputInterface {
+	label?: string;
+	type: string;
+	placeholder: string;
+	onChange?: () => void;
 }
