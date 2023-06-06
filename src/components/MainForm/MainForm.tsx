ab9fe8b3230b18569c3483e98c1a82775c9971f4
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-06-06 03:01:45 +0300

    feat: redirect on click btn

--- src/components/MainForm/MainForm.tsx
+++ src/components/MainForm/MainForm.tsx
@@ -1,15 +1,23 @@
-import { redirect } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Input from '../Input';
 import Button from '../Buttons';
+import Label from '../Label';
+import st from './MainForm.module.scss';
 
 export const MainForm = () => {
+	const navigate = useNavigate();
 	const handler = (e: React.MouseEvent<HTMLButtonElement>) => {
 		e.preventDefault();
-		console.log('qq');
+		navigate('/one');
 	};
 	return (
-		<form>
-			<Input label='Номер телефона' type='number' placeholder='+7 999 999-99-99' />
+		<form className={st.form}>
+			<Label text='Номер телефона'>
+				<Input type='number' placeholder='+7 999 999-99-99' />
+			</Label>
+			<Label text='Email'>
+				<Input type='email' placeholder='tim.jennings@example.com' />
+			</Label>
 			<Button text='Начать' id='button-start' handler={handler} />
 		</form>
 	);
