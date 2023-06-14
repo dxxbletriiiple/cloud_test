7bc9239aa4ee4041e2301d900c7aeeb0a1a13e85
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-06-15 02:19:38 +0300

    edit: label for other pages

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -1,9 +1,10 @@
+import cn from 'classnames';
 import { ILabel } from '../../interfaces';
 import st from './Label.module.scss';
 
-export const Label = ({ text, children }: ILabel) => {
+export const Label = ({ text, children, field }: ILabel) => {
 	return (
-		<label className={st.label}>
+		<label className={cn(st.label, { [st.field]: field })}>
 			<span>{text}</span>
 			{children}
 		</label>
