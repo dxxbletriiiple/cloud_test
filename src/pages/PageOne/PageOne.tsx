d9f8d21c9f917d69a0ca0eac4a6eee7052ee8998
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-06-14 15:09:12 +0300

    add: page one

---
+++ src/pages/PageOne/PageOne.tsx
@@ -0,0 +1,10 @@
+import Progress from '../../components/Progress';
+import st from './PageOne.module.scss';
+
+export const PageOne = () => {
+	return (
+		<section className={st.one}>
+			<Progress />
+		</section>
+	);
+};
