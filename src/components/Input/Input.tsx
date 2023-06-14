323144cf52b344d5bdd72bb82ff8fa0bd7f8cf42
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-06-15 02:20:13 +0300

    edit: input for other pages except main

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,10 +1,16 @@
+import cn from 'classnames';
 import { IInput } from '../../interfaces';
 import st from './Input.module.scss';
 
 export const Input = (props: IInput) => {
 	return (
 		<>
-			<input className={st.input} {...props} />
+			<input
+				className={cn(st.input, {
+					[st.white]: props.id?.includes('field'),
+				})}
+				{...props}
+			/>
 		</>
 	);
 };
